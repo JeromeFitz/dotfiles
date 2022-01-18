dcca467df9ecb8fb1c867690c676394754ef632b
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-01-18 17:51:51 -0500

    👷  changelog, lint-staged updates

--- changelog.config.mjs
+++ changelog.config.mjs
@@ -1,9 +1,41 @@
-import config from '@jeromefitz/git-cz/gitmoji'
 import isCI from 'is-ci'
-!isCI && require('dotenv').config({ path: './.env' })
 
-const enabled = process.env.GIT_CZ || true
+if (!isCI) {
+  const dotenv = await import('dotenv')
+  dotenv.config({ path: './.env' })
+}
 
-const changelog = { ...config, enabled }
+const isOverride = process.env.GIT_CZ__OVERRIDE_TEST || false
+
+const enabled = isOverride
+
+const _types = {}
+
+const commit = isOverride
+  ? {
+      after: { branchName: ' ', emoji: '  ', scope: ') ' },
+      before: { branchName: '', emoji: '', scope: '(' },
+      format: '{emoji}{scope}{branchName}{subject}',
+      maxMessageLength: 64,
+      minMessageLength: 3,
+      questions: [
+        'branchFlag',
+        'commitBreakingFlag',
+        'commitBreaking',
+        // 'commitScopes',
+        'commitTypes',
+        'commitSubject',
+        'commitBodyFlag',
+        'commitBody',
+      ],
+      scopes: [],
+    }
+  : {}
+
+const branch = {}
+
+const types = isOverride ? _types : {}
+
+const changelog = { branch, commit, enabled, types }
 
 export default changelog
