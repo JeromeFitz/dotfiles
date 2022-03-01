45c062269c568aa65e5c0ad339f842bc06c0e959
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-03-01 11:23:33 -0500

    👷  new release config

--- release.config.cjs
+++ release.config.cjs
@@ -1,47 +1,23 @@
-const release = require('@jeromefitz/semantic/release.config.cjs').default
+const { getConfig } = require('@jeromefitz/semantic')
 const isCI = require('is-ci')
+
 !isCI && require('dotenv').config({ path: './.env' })
 
 const { name } = require('./package.json')
 
-const plugins = release.plugins
-
 const branches = [{ name: 'main' }, { name: 'develop', prerelease: 'develop' }]
 
-// const ci = true
-// const dryRun = false
-const _extends = ['semantic-release-commit-filter']
-
-const pluginsOverride = [
-  [
-    '@semantic-release/npm',
-    {
-      npmPublish: false,
-    },
-  ],
-  [
-    '@semantic-release/github',
-    { labels: false, releasedLabels: false, successComment: false },
-  ],
-]
-
-/**
- * @refactor This mutates plugins which is not ideal
- */
-plugins.map((plugin, pluginIndex) => {
-  const pluginName = plugin[0]
-  pluginsOverride.map((pluginOverride) => {
-    pluginName === pluginOverride[0] ? (plugins[pluginIndex] = pluginOverride) : null
-  })
-})
-
-module.exports = {
-  ...release,
+const configPassed = {
   branches,
-  // ci,
-  // dryRun,
-  extends: _extends,
-  plugins,
+  contributorsProhibitList: {
+    email: [],
+    login: ['BotJerome', 'JeromeFitz'],
+  },
+  enableNpm: false,
   repositoryUrl: `https://github.com/${name.replace('@', '')}`,
   tagFormat: 'v${version}',
 }
+
+const config = getConfig(configPassed)
+
+module.exports = config
