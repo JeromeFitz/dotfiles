e83955051eb835e7ce53cb7b81a905ac195e9e34
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-10-04 09:33:11 -0400

    🐛  (outlook) refreshOutlook for 🍏 osx
    Not the greatest "fix" but aside from a CRON job
     this can be run quickly to get things aligned.

--- config/zsh.alias.zsh
+++ config/zsh.alias.zsh
@@ -6,3 +6,7 @@ alias dotfiles='cd ~/.dotfiles'
 alias dotfilesc='dotfiles && code .'
 
 alias getGitConfig='echo "[ git ] 📛  user.name:  $(git config --get user.name)" && echo "[ git ] 📧  user.email: $(git config --get user.email)"'
+
+# @note(to-be-removed) Issue with MacOS Monterrey
+# @note(feature) Should this be CRON?
+alias refreshOutlook='launchctl stop com.apple.CalendarAgent && launchctl start com.apple.CalendarAgent'
